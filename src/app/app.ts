316f776a16fe467f4708f419264fcb337715ed5c
Szabo Patrik Peter <h377671@stud.u-szeged.hu>
2026-05-02 19:49:43 +0200

    szakdolgozat és kiszervezett footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { Footer } from './shared/footer/footer';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, NavbarComponent, Footer, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/shared/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+
+}
